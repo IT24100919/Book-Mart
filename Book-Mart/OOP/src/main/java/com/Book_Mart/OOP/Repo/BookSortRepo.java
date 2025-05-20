de8d68deffc27d1c78d49bf23ed0dfa994579490
IT24100837 <it24100837@my.sliit.lk>
2025-05-20 12:11:36 +0530

    Delete Book-Mart/OOP directory

--- Book-Mart/OOP/src/main/java/com/Book_Mart/OOP/Repo/BookSortRepo.java
+++
@@ -1,9 +0,0 @@
-package com.Book_Mart.OOP.Repo;
-
-import com.Book_Mart.OOP.Model.BookSort;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookSortRepo extends JpaRepository<BookSort, Long> {
-}
